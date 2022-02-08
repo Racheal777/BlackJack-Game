0971df77ad5a0f6464a63b8aa3a60ce104aca89b
Racheal Kuranchie <kuranchieracheal@gmail.com>
2022-02-08 20:17:19 +0000

    added a loop

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 // //Card game
 
-let firstCard = 10
-let secondCard = 4
+let firstCard = getRandoms()
+let secondCard = getRandoms()
 let cards = [firstCard, secondCard]
 let hasBlackjack = false
 let message = ""
@@ -14,7 +14,14 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardEl = document.getElementById("card-el")
 
-//function to call the render game to action
+
+//function to generate random numbers
+
+function getRandoms() {
+    return 6
+    
+}
+//function to call the render game function to action
 function startGame() {
     renderGame()
 }
@@ -22,7 +29,14 @@ function startGame() {
 
 //function to display cards, the sum and whether you have won or not
 function renderGame(){
-    cardEl.textContent = "Cards : " + cards[0] + " " + cards[1]
+    
+
+    //creating a forloop to loop through the cards
+    for(let i = 0; i < cards.length; i++){
+        cardEl.textContent +=  cards[i] + " "
+    }
+
+
     sumEl.textContent = "Sum :" + sum
     if(sum <= 20){
         message = "Do you want to pick another card ? "
@@ -43,11 +57,87 @@ function renderGame(){
 //function to draw new card
 
 function newCard() {
-    let card = 9
+    let card = getRandoms()
     sum += card
 
+    //adding the value of the card to the cards
+    cards.push(card)
      renderGame()
     
 }
 
 
+// let ca = [1, 3]
+
+// ca.push(6)
+
+// console.log(ca)
+
+// ca.pop()
+// console.log(ca)
+//for loop
+
+
+     //start            //finish  //jump
+// for (let index = 10; index < 21; index++) {
+    
+//     console.log(index)
+    
+// }
+
+// let messages = ['hello', 'hi', 'hey', 'what', 'mee', 'you']
+
+// let cardss = [7, 8, 15, 20]
+// for(let i = 0; i < message.length; i++){
+
+//     console.log(messages[i])
+// }
+
+// for(let i = 0; i < cardss.length; i++){
+
+//     console.log(cardss[i])
+// }
+
+// let sentence = ['hello', "my", "name", "is", "Racheal"]
+
+// const greet = document.getElementById("greatings-el")
+
+// for (let i = 0; i < sentence.length; i++) {
+    
+//     console.log(sentence[i])
+
+// greet.textContent += sentence[i] + " "
+
+// }
+
+// let player1 = 108;
+// let player2 = 104;
+
+// function fastPlayer() {
+//     if(player1 < player2){
+//         return player1
+//     }else if(player2 < player1){
+//         return player2
+//     }else{
+//         return player1
+//     }
+    
+// }
+
+
+// let fastTime = fastPlayer()
+// console.log(fastTime)
+
+// function totalRace() {
+//     return player1 + player2
+    
+// }
+
+// // console.log(totalRace(player1,player2))
+
+// let allTime = totalRace()
+// console.log(allTime)
+
+let numbers = Math.random() * 5
+
+console.log(numbers);
